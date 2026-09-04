047162ab4de87b285586ab495a2d936aa2c468e1
MarlonMaiaa <marlonnmaiaa@gmail.com>
2026-09-03 21:17:30 -0300

    add buscar a parti do ano

--- screnmach/src/main/java/br/com/alura/screnmach/principal/Principal.java
+++ screnmach/src/main/java/br/com/alura/screnmach/principal/Principal.java
@@ -8,6 +8,8 @@ import br.com.alura.screnmach.service.ConsumoApi;
 import br.com.alura.screnmach.service.ConverteDados;
 import tools.jackson.databind.jsonFormatVisitors.JsonFormatVisitable;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -68,6 +70,23 @@ public class Principal {
                 ).collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+        System.out.println("A  parti de que ano deseja lista os episodios");
+        var ano = scanner.nextInt();
+        scanner.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatterBr = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada:" + e.getTemporada() +
+                        "Episodio:" + e.getTitulo() +
+                        "Data de lancamento:" + e.getDataLancamento().format(formatterBr)
+                ));
+
+
 
 
 
